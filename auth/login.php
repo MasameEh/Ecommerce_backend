1a7ca7cdfa8f3a3ede088bb533bb0966ea66bb5c
MasameEh <samehms116@gmail.com>
2024-07-29 12:42:00 +0300

    add login and verifycode
    Signed-off-by: MasameEh <samehms116@gmail.com>

---
+++ auth/login.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../connect.php";
+
+$email          = filterRequest("email");
+$password       = sha1($_POST["password"]);
+
+$stmt = $con->prepare("SELECT * From users Where users_email = ? AND users_password = ?");
+
+$stmt->execute(array($email, $password));
+
+$count = $stmt->rowCount();
+
+if($count > 0)
+{
+    echo json_encode(array("status" => "success"));
+}
+else
+{
+    printFailure("EMAIL OR PASSWORD IS NOT CORRECT");
+}
--- auth/signup.php
+++ auth/signup.php
@@ -2,14 +2,14 @@
 
 include "../connect.php";
 
-$username = filterRequest("username");
-$email    = filterRequest("email");
-$password = sha1("password");
-$phone    = filterRequest("phone");
-$verifycode    = "0";
+$username       = filterRequest("username");
+$email          = filterRequest("email");
+$password       = sha1($_POST["password"]);
+$phone          = filterRequest("phone");
+$verifycode     = rand(10000, 99999);
 
 
-$stmt = $con->prepare("Select * From users Where users_email = ? OR users_phone = ?");
+$stmt = $con->prepare("SELECT * From users Where users_email = ? OR users_phone = ?");
 
 $stmt->execute(array($email, $phone));
 
@@ -22,12 +22,13 @@ if($count > 0)
 else
 {
     $data = array(
-        "users_name" => "$username",
-        "users_email" => "$email",
-        "users_password" => "$password",
-        "users_phone" => "$phone",
-        "users_verifycode" => "0",        
+        "users_name" => $username,
+        "users_email" => $email,
+        "users_password" => $password,
+        "users_phone" => $phone,
+        "users_verifycode" => $verifycode,        
     );
+    //sendEmail($email, "Ecommerce Verification Code", "Please enter the confirmation code below on Ecommerce to finish the verification process: $verifycode");
 
     insertData("users", $data);
 }
