6a2dbc1749ffee31dee56f0d83bd652ed903796a
MasameEh <samehms116@gmail.com>
2024-09-29 16:09:07 +0300

    edit login
    Signed-off-by: MasameEh <samehms116@gmail.com>

--- auth/login.php
+++ auth/login.php
@@ -5,17 +5,13 @@ include "../connect.php";
 $email          = filterRequest("email");
 $password       = sha1($_POST["password"]);
 
-$stmt = $con->prepare("SELECT * From users Where users_email = ? AND users_password = ?");
+// $stmt = $con->prepare("SELECT * From users Where users_email = ? AND users_password = ?");
+// $stmt->execute(array($email, $password));
+// $count = $stmt->rowCount();
+// if($count > 0){
+//     echo json_encode(array("status" => "success"));
+// }else{
+//     printFailure("EMAIL OR PASSWORD IS NOT CORRECT");
+// }
 
-$stmt->execute(array($email, $password));
-
-$count = $stmt->rowCount();
-
-if($count > 0)
-{
-    echo json_encode(array("status" => "success"));
-}
-else
-{
-    printFailure("EMAIL OR PASSWORD IS NOT CORRECT");
-}
+getData("users", "users_email = ? AND users_password = ?", array($email, $password));
